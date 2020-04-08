4af51f651ebc886fe3aad96f904aa9ddd7caeac4
Rubhi Aulia Tirta <rubhiauliatirta@Rubhis-MacBook-Air.local>
2020-04-09 01:24:04 +0700

    add comment at app.js

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ mongo.connect(function(err){
     app.use(express.json())
     app.use(express.urlencoded({extended: false}))
     app.use("/", require("./routes"))
+    /**
+     * require("./router") ini penting untuk diletakkan didalam callback mongo.connect ini
+     * kerena kalau di require sebelumnya nya, getDatabase akan return undefined sehingga
+     * code di bagian models menjadi error
+     */
 
     app.listen(PORT, function(){
       console.log("server is running on PORT " + PORT)
